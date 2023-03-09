072c6354ebf2a6c1a1abe342e288eb6255f96adb
Jess Mark Sevilleja <jess@clinica.dev>
2023-03-10 07:08:56 +0800

    added button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 interface Props {
   text: string;
-  onClick: () => void;
+  onClick?: () => void;
   className?: string;
 }
 
 const Button: React.FC<Props> = ({ text, onClick, className }) => (
   <button
-    className={`inline-flex items-center px-4 py-2 text-sm font-medium bg-primary rounded hover:bg-secondary text-white ${className}`}
+    className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded my-4 ${className}`}
     onClick={onClick}
   >
     {text}
